eba7f8fdc899e143f5409926f444c07d2696e35a
Cataline Baert <snuf_kin@yahoo.com>
2019-11-20 20:26:55 +0100

    move show route after new route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'todos', to: 'todos#index'
-  get 'todos/:id', to: 'todos#show'
+
   get 'todos/new', to: 'todos#new'
   post 'todos', to: 'todos#create'
+
+  get 'todos/:id', to: 'todos#show'
 end
