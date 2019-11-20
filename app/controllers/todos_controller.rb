8f12b6132799d6eecde00d77a7daf29f8f038c6d
Cataline Baert <snuf_kin@yahoo.com>
2019-11-20 14:26:24 +0100

    create new private method to print message depending on completed status and define new variable containing the message

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -5,5 +5,12 @@ class TodosController < ApplicationController
 
   def show
     @todo = Todo.find(params[:id])
+    @message = completed_message(@todo)
+  end
+
+  private
+
+  def completed_message(todo)
+    todo.completed ? 'This task is completed' : 'This task is not completed yet'
   end
 end
