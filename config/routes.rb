2ec5b472829dcd527c18aef120c22528ca63cd14
Cataline Baert <snuf_kin@yahoo.com>
2019-11-22 15:34:34 +0100

    refactor using resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
-  get 'todos', to: 'todos#index'
+  # get 'todos', to: 'todos#index'
 
-  get 'todos/new', to: 'todos#new'
-  post 'todos', to: 'todos#create'
+  # get 'todos/new', to: 'todos#new'
+  # post 'todos', to: 'todos#create'
 
-  get 'todos/:id', to: 'todos#show'
+  # get 'todos/:id', to: 'todos#show'
 
-  get 'todos/:id/edit', to: 'todos#edit'
-  patch 'todos/:id', to: 'todos#update'
+  # get 'todos/:id/edit', to: 'todos#edit'
+  # patch 'todos/:id', to: 'todos#update'
 
-  delete 'todos/:id', to: 'todos#delete'
+  # delete 'todos/:id', to: 'todos#delete'
+  resources :todos
 end
