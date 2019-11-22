20fd350e5475dd2014538c54e067398de324d6cc
Cataline Baert <snuf_kin@yahoo.com>
2019-11-22 21:14:10 +0100

    create seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# frozen_string_literal: true
+
+puts 'Cleaning database...'
+Todo.destroy_all
+
+puts 'Creating todos...'
+Todo.create(
+  title: 'Laundry',
+  details: 'Do not mix colors!'
+)
+Todo.create(
+  title: 'Studying',
+  details: 'A lot of flashcard to do',
+  completed: true
+)
